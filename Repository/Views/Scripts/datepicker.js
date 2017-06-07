8d7e567de4e8a990f1f47d063632bd87c071b83c
epf <eric.pfirsch@med-link.org>
2017-06-07 16:54:53 +0200

    3.2

--- Repository/Views/Scripts/datepicker.js
+++ Repository/Views/Scripts/datepicker.js
@@ -4,8 +4,7 @@
 		prevText: '&#x3c;zurück',
 		nextText: 'Vor&#x3e;',
 		currentText: 'heute',
-		monthNames: ['Januar','Februar','März','April','Mai','Juni',
-		'Juli','August','September','Oktober','November','Dezember'],
+		monthNames: ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
 		monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
 		'Jul','Aug','Sep','Okt','Nov','Dez'],
 		dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
@@ -17,14 +16,14 @@
 		showMonthAfterYear: false,
 		yearSuffix: ''};
 });
+
 jQuery(function($){
 	$.datepicker.regional['es'] = {
 		closeText: 'Cerrar',
 		prevText: '&#x3c;Ant',
 		nextText: 'Sig&#x3e;',
 		currentText: 'Hoy',
-		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
-		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
 		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
 		'Jul','Ago','Sep','Oct','Nov','Dic'],
 		dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
@@ -43,8 +42,7 @@ jQuery(function($){
 		prevText: '&#x3c;Préc',
 		nextText: 'Suiv&#x3e;',
 		currentText: 'Courant',
-		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
-		'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
 		monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
 		'Jul','Aoû','Sep','Oct','Nov','Déc'],
 		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
@@ -60,11 +58,10 @@ jQuery(function($){
 jQuery(function($){
 	$.datepicker.regional['en'] = {
 		closeText: 'Done',
-		prevText: 'Prev',
-		nextText: 'Next',
+		prevText: '&#x3c;Prev',
+		nextText: 'Next&#x3e;',
 		currentText: 'Today',
-		monthNames: ['January','February','March','April','May','June',
-		'July','August','September','October','November','December'],
+		monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
 		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
 		'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 		dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
@@ -83,8 +80,7 @@ jQuery(function($){
 			prevText: '&#x3c;Prec',
 			nextText: 'Succ&#x3e;',
 			currentText: 'Oggi',
-			monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno',
-					'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+			monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
 			monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu',
 					'Lug','Ago','Set','Ott','Nov','Dic'],
 			dayNames: ['Domenica','Luned&#236','Marted&#236','Mercoled&#236','Gioved&#236','Venerd&#236','Sabato'],
@@ -96,4 +92,43 @@ jQuery(function($){
 			isRTL: false,
 			showMonthAfterYear: false,
 			yearSuffix: ''};
+});
+
+
+jQuery(function ($) {
+    $.datepicker.regional['nb'] = {
+        closeText: 'Ferdig',
+        prevText: '&#x3c;Forr',
+        nextText: 'Neste&#x3e;',
+        currentText: 'I dag',
+        monthNames: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni','Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
+        monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun','Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'],
+        dayNames: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+        dayNamesShort: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'],
+        dayNamesMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+        weekHeader: 'Uke',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+});
+
+jQuery(function ($) {
+    $.datepicker.regional['pt'] = {
+        closeText: 'Fechar',
+        prevText: '&#x3c;Anterior',
+        nextText: 'Próximo&#x3e;',
+        currentText: 'Hoje',
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+        dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+        dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+        weekHeader: 'Sm',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
 });
