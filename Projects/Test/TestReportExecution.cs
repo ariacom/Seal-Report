ba15afe802dedf12ebedb38d8632acfde606a541
epf <ep@ariacom.com>
2018-05-10 13:12:01 +0200

    4.0

--- Projects/Test/TestReportExecution.cs
+++ Projects/Test/TestReportExecution.cs
@@ -26,49 +26,6 @@ namespace Test
         }
 
         [TestMethod]
-        public void ExecutionWithExternalDataTables()
-        {
-            //Get Data Table from another source
-            string sql = @"
-SELECT DISTINCT
-  DateSerial(DatePart('yyyy',[Orders.OrderDate]), 1, 1) AS C0,
-  Products.CategoryID AS C1,
-  Customers.Country AS C2,
-  999999 AS C3
-FROM 
-(Products INNER JOIN 
-([Order Details] INNER JOIN 
-(Orders INNER JOIN Customers
- ON Customers.CustomerID = Orders.CustomerID)
- ON Orders.OrderID = [Order Details].OrderID)
- ON Products.ProductID = [Order Details].ProductID)
-";
-
-            var connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\ProgramData\Seal Report Repository\Databases\Northwind.mdb;Persist Security Info=False");
-            var command = connection.CreateCommand();
-            command.CommandText = sql;
-            var adapter = new OleDbDataAdapter(command);
-            var newTable = new DataTable();
-            adapter.Fill(newTable);
-
-            //Data table must have the same column definition as the one defined in the report
-            var repository = Repository.Create();
-            var report = Report.LoadFromFile(@"C:\ProgramData\Seal Report Repository\Reports\Samples\03-Cross tab - Simple chart (Orders).srex", repository);
-
-            //Set data table to the report model (here there is only 1 model)
-            report.Models[0].ResultTable = newTable;
-            //Set report in Render only mode so the Result table is not loaded during the execution 
-            report.RenderOnly = true;
-            //Note that if model.ResultTable is null, the table will be loaded anyway
-
-            var execution = new ReportExecution() { Report = report };
-            execution.Execute();
-            while (report.Status != ReportStatus.Executed) System.Threading.Thread.Sleep(100);
-            string result = execution.GenerateHTMLResult();
-            Process.Start(result);
-        }
-
-        [TestMethod]
         public void CreationAndExecution()
         {
             var repository = Repository.Create();
