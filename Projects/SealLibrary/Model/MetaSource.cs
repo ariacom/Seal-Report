05c7cc61c9811fc56c3c1ea1b921f2f530776552
epf <ep@ariacom.com>
2018-09-17 17:21:15 +0200

    4.1

--- Projects/SealLibrary/Model/MetaSource.cs
+++ Projects/SealLibrary/Model/MetaSource.cs
@@ -488,17 +488,33 @@ namespace Seal.Model
             return "[]";
         }
 
+
+        static string[] MSSQLKeywords = new string[] { "ADD", "EXTERNAL", "PROCEDURE", "ALL", "FETCH", "PUBLIC", "ALTER", "FILE", "RAISERROR", "AND", "FILLFACTOR", "READ", "ANY", "FOR", "READTEXT", "AS", "FOREIGN", "RECONFIGURE", "ASC", "FREETEXT", "REFERENCES", "AUTHORIZATION", "FREETEXTTABLE", "REPLICATION", "BACKUP", "FROM", "RESTORE", "BEGIN", "FULL", "RESTRICT", "BETWEEN", "FUNCTION", "RETURN", "BREAK", "GOTO", "REVERT", "BROWSE", "GRANT", "REVOKE", "BULK", "GROUP", "RIGHT", "BY", "HAVING", "ROLLBACK", "CASCADE", "HOLDLOCK", "ROWCOUNT", "CASE", "IDENTITY", "ROWGUIDCOL", "CHECK", "IDENTITY_INSERT", "RULE", "CHECKPOINT", "IDENTITYCOL", "SAVE", "CLOSE", "IF", "SCHEMA", "CLUSTERED", "IN", "SECURITYAUDIT", "COALESCE", "INDEX", "SELECT", "COLLATE", "INNER", "SEMANTICKEYPHRASETABLE", "COLUMN", "INSERT", "SEMANTICSIMILARITYDETAILSTABLE", "COMMIT", "INTERSECT", "SEMANTICSIMILARITYTABLE", "COMPUTE", "INTO", "SESSION_USER", "CONSTRAINT", "IS", "SET", "CONTAINS", "JOIN", "SETUSER", "CONTAINSTABLE", "KEY", "SHUTDOWN", "CONTINUE", "KILL", "SOME", "CONVERT", "LEFT", "STATISTICS", "CREATE", "LIKE", "SYSTEM_USER", "CROSS", "LINENO", "TABLE", "CURRENT", "LOAD", "TABLESAMPLE", "CURRENT_DATE", "MERGE", "TEXTSIZE", "CURRENT_TIME", "NATIONAL", "THEN", "CURRENT_TIMESTAMP", "NOCHECK", "TO", "CURRENT_USER", "NONCLUSTERED", "TOP", "CURSOR", "NOT", "TRAN", "DATABASE", "NULL", "TRANSACTION", "DBCC", "NULLIF", "TRIGGER", "DEALLOCATE", "OF", "TRUNCATE", "DECLARE", "OFF", "TRY_CONVERT", "DEFAULT", "OFFSETS", "TSEQUAL", "DELETE", "ON", "UNION", "DENY", "OPEN", "UNIQUE", "DESC", "OPENDATASOURCE", "UNPIVOT", "DISK", "OPENQUERY", "UPDATE", "DISTINCT", "OPENROWSET", "UPDATETEXT", "DISTRIBUTED", "OPENXML", "USE", "DOUBLE", "OPTION", "USER", "DROP", "OR", "VALUES", "DUMP", "ORDER", "VARYING", "ELSE", "OUTER", "VIEW", "END", "OVER", "WAITFOR", "ERRLVL", "PERCENT", "WHEN", "ESCAPE", "PIVOT", "WHERE", "EXCEPT", "PLAN", "WHILE", "EXEC", "PRECISION", "WITH", "EXECUTE", "PRIMARY", "WITHIN GROUP", "EXISTS", "PRINT", "WRITETEXT", "EXIT", "PROC" };
+
+        string[] getKeywords(DatabaseType dbType)
+        {
+            if (dbType == DatabaseType.MSSQLServer) return MSSQLKeywords;
+            //TODO for other db types
+            return new string[] { "" };
+        }
+
         public string GetTableName(string rawName)
         {
             string delimiters = getDelimiters(Connection.DatabaseType);
-            if (!rawName.StartsWith("[") && !rawName.EndsWith("]") && rawName.IndexOfAny(" '\"-$-".ToCharArray()) != -1) return string.Format("{0}{1}{2}", delimiters[0], rawName, delimiters[1]);
+            var keywords = getKeywords(Connection.DatabaseType);
+            if ( (!rawName.StartsWith("[") && !rawName.EndsWith("]") && rawName.IndexOfAny(" '\"-$-".ToCharArray()) != -1) 
+                || keywords.Contains(rawName.ToUpper()))
+                return string.Format("{0}{1}{2}", delimiters[0], rawName, delimiters[1]);
             return rawName;
         }
 
         public string GetColumnName(string rawName)
         {
             string delimiters = getDelimiters(Connection.DatabaseType);
-            if (!rawName.StartsWith("[") && !rawName.EndsWith("]") && rawName.IndexOfAny(" '\"-".ToCharArray()) != -1) return string.Format("{0}{1}{2}", delimiters[0], rawName, delimiters[1]);
+            var keywords = getKeywords(Connection.DatabaseType);
+            if ((!rawName.StartsWith("[") && !rawName.EndsWith("]") && rawName.IndexOfAny(" '\"-$-".ToCharArray()) != -1)
+                || keywords.Contains(rawName.ToUpper()))
+                return string.Format("{0}{1}{2}", delimiters[0], rawName, delimiters[1]);
             return rawName;
         }
 
