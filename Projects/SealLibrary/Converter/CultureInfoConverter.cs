bd4ca35cf761b677dfb44107ebd151b843fdb586
Seal Report <ep@ariacom.com>
2018-10-06 10:00:26 +0200

    4.1

--- Projects/SealLibrary/Converter/CultureInfoConverter.cs
+++ Projects/SealLibrary/Converter/CultureInfoConverter.cs
@@ -2,12 +2,9 @@
 // Copyright (c) Seal Report, Seal Report, Eric Pfirsch (sealreport@gmail.com), http://www.sealreport.org.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. http://www.apache.org/licenses/LICENSE-2.0..
 //
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.ComponentModel; 
-using Seal.Model;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace Seal.Converter
