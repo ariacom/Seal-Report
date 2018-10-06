07d9fdb40189181f114e99122882dd5464f5d8a7
Seal Report <ep@ariacom.com>
2018-10-06 09:51:55 +0200

    4.1

--- Projects/SealLibrary/Converter/CultureInfoConverter.cs
+++ Projects/SealLibrary/Converter/CultureInfoConverter.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.ComponentModel;
+using System.ComponentModel; 
 using Seal.Model;
 using System.Globalization;
 
