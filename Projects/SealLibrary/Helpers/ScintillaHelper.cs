add01a8e6e57af8f829dff2ff48dc4631997638e
epf <ep@ariacom.com>
2018-10-28 16:50:23 +0100

    4.1

--- Projects/SealLibrary/Helpers/ScintillaHelper.cs
+++ Projects/SealLibrary/Helpers/ScintillaHelper.cs
@@ -164,6 +164,34 @@ namespace Seal.Helpers
                 // User2 = 5
                 scintilla.SetKeywords(5, @"sys objects sysobjects ");
             }
+            else if (lex == Lexer.Container)
+            {
+                // Reset the styles
+                scintilla.StyleResetDefault();
+                scintilla.Styles[Style.Default].Font = "Courier New";
+                scintilla.Styles[Style.Default].Size = 10;
+                scintilla.StyleClearAll();
+
+                // Set the SQL Lexer
+                scintilla.Styles[ScintillaRestrictionLexer.StyleDefault].ForeColor = Color.Black;
+                scintilla.Styles[ScintillaRestrictionLexer.StyleKeyword].ForeColor = Color.Olive;
+                scintilla.Styles[ScintillaRestrictionLexer.StyleIdentifier].ForeColor = Color.Black;
+                scintilla.Styles[ScintillaRestrictionLexer.StyleNumber].ForeColor = Color.Maroon;
+                scintilla.Styles[ScintillaRestrictionLexer.StyleString].ForeColor = Color.Red;
+                scintilla.Styles[ScintillaRestrictionLexer.StyleRestriction].ForeColor = Color.Teal;
+
+                if (scintilla.Tag == null)
+                {
+                    ScintillaRestrictionLexer sealLexer = new ScintillaRestrictionLexer(@"all and any between cross exists in inner is join left like not null or outer pivot right some unpivot add alter as authorization backup begin bigint binary bit break browse bulk by cascade case catch check checkpoint close clustered column commit compute constraint containstable continue create current cursor cursor database date datetime datetime2 datetimeoffset dbcc deallocate decimal declare default delete deny desc disk distinct distributed double drop dump else end errlvl escape except exec execute exit external fetch file fillfactor float for foreign freetext freetexttable from full function goto grant group having hierarchyid holdlock identity identity_insert identitycol if image index insert int intersect into key kill lineno load merge money national nchar nocheck nocount nolock nonclustered ntext numeric nvarchar of off offsets on open opendatasource openquery openrowset openxml option order over percent plan precision primary print proc procedure public raiserror read readtext real reconfigure references replication restore restrict return revert revoke rollback rowcount rowguidcol rule save schema securityaudit select set setuser shutdown smalldatetime smallint smallmoney sql_variant statistics table table tablesample text textsize then time timestamp tinyint to top tran transaction trigger truncate try union unique uniqueidentifier update updatetext use user values varbinary varchar varying view waitfor when where while with writetext xml go");
+                    scintilla.StyleNeeded += new EventHandler<StyleNeededEventArgs>(delegate (object sender, StyleNeededEventArgs e)
+                    {
+                        var startPos = scintilla.GetEndStyled();
+                        var endPos = e.Position;
+
+                        sealLexer.Style(scintilla, startPos, endPos);
+                    });
+                }
+            }
 
             //First initialization
             if (scintilla.Tag == null)
