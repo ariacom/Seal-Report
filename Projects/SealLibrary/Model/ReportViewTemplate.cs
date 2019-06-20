401a867ae0ffb4fa55775cf6b843bef184bf13a1
epf <ep@ariacom.com>
2019-06-20 17:13:50 +0200

    5.0

--- Projects/SealLibrary/Model/ReportViewTemplate.cs
+++ Projects/SealLibrary/Model/ReportViewTemplate.cs
@@ -18,7 +18,6 @@ namespace Seal.Model
         public const string DataTableName = "Data Table";
         public const string PageTableName = "Page Table";
         public const string ChartNVD3Name = "Chart NVD3";
-        public const string KPIName = "KPI";
         public const string ChartJSName = "Chart JS";
         public const string ChartPlotlyName = "Chart Plotly";
         public const string ModelContainerName = "Model Container";
