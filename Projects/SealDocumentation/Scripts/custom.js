788cf5d716f80abd6a1a17e6d7b74ea78396e320
epf <ep@ariacom.com>
2019-06-24 11:55:56 +0200

    5.0

---
+++ Projects/SealDocumentation/Scripts/custom.js
@@ -0,0 +1,66 @@
+(function($) {
+    "use strict";
+
+    /* ==============================================
+    ANIMATION -->
+    =============================================== */
+
+    new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    }).init();
+
+
+    /* ==============================================
+    SCROLL -->
+    =============================================== */
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* ==============================================
+    SCROLLSPY -->
+    =============================================== */
+
+    $('body').scrollspy({
+        target: '.docs-sidebar'
+    });
+
+    $('[data-spy="scroll"]').each(function () {
+      var $spy = $(this).scrollspy('refresh')
+    })
+
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $(document).ready(function() {
+        // Target your .container, .wrapper, .post, etc.
+        $(".media").fitVids();
+    });
+
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $('.docs-sidebar>nav>li>a').click(function() {
+        $('.docs-sidebar>nav>li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
+})(jQuery);
