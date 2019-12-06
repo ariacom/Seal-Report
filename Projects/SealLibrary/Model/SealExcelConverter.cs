f941c244f9b5e63abccc39824fb72a9920afdbb8
epf <ep@ariacom.com>
2019-12-06 08:06:33 +0100

    5.1

--- Projects/SealLibrary/Model/SealExcelConverter.cs
+++ Projects/SealLibrary/Model/SealExcelConverter.cs
@@ -49,7 +49,7 @@ namespace Seal.Model
                 try
                 {
                     Assembly currentAssembly = AppDomain.CurrentDomain.Load("SealConverter");
-                    Type t = currentAssembly.GetType("SealExcelConverter.ExcelConverter", true);
+                    Type t = currentAssembly.GetType("Seal.Converter.ExcelConverter", true);
                     Object[] args = new Object[] { };
                     result = (SealExcelConverter)t.InvokeMember(null, BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance | BindingFlags.CreateInstance, null, null, args);
                     result.ApplicationPath = applicationPath;
