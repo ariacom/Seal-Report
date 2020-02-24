5a8e3cd6901d53443fe2dbbf1db2553883d296d4
Seal Report <ep@ariacom.com>
2020-02-24 10:49:49 +0100

    5.5

--- Projects.NETCore/SealLibrary/Model/ReportModel.cs
+++ Projects.NETCore/SealLibrary/Model/ReportModel.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright 2015 (c) Seal Report, Eric Pfirsch (sealreport@gmail.com), http://www.sealreport.org.
+// Copyright 2015 (c) Seal Report (sealreport@gmail.com), http://www.sealreport.org.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. http://www.apache.org/licenses/LICENSE-2.0
 //
 
