6f702b41d9d419843681376ed4735a7797f37601
Seal Report <ep@ariacom.com>
2020-10-27 09:54:24 +0100

    6.0

--- Projects/SealLibrary/Model/Repository.cs
+++ Projects/SealLibrary/Model/Repository.cs
@@ -642,7 +642,7 @@ namespace Seal.Model
             get
             {
 #if NETCOREAPP
-                return Path.Combine(RepositoryPath, "Assemblies\\NETCore"); 
+                return Path.Combine(RepositoryPath, Path.Combine("Assemblies","NETCore")); 
 #else
                 return Path.Combine(RepositoryPath, "Assemblies");
 #endif
